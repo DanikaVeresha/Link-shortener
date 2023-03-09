8a174b6206cec325c74b633899c8cb5a04321166
daria.ch.prozorro <daria.ch.prozorro@gmail.com>
2023-03-09 13:11:28 +0200

    hw_23

--- application.py
+++ application.py
@@ -4,41 +4,34 @@ import string
 from aiohttp import web
 import motor.motor_asyncio
 
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    f'mongodb://root:example@{os.environ.get("DB_HOST","localhost")}:27017')
+db = client['redirecter']
+collection = db['redirects']
+
+
+def html_response(document):
+    file = open(document, "r")
+    return web.Response(text=file.read(), content_type='text/html')
+
 
 async def index_page(request):
-    return web.Response(text="""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Redirecter</title>
-</head>
-<body>
-<h1>Redirecter</h1>
-<p>Redirects to a long URL based on a short URL,</p>
-<form action="/" method="post">
-    <input type="text" name="lond_url" placeholder="Long URL" id="long_url" required>
-    <input type="submit" value="Short id">
-</form>
-</body>
-</html>""", content_type='text/html')
+    return html_response('redirecter.html')
 
 
 async def recieve_url(request):
     data = await request.post()
     long_url = data['lond_url']
-    client = motor.motor_asyncio.AsyncIOMotorClient(f'mongodb://root:example@{os.environ.get("DB_HOST","localhost")}:27017')
-    db = client['redirecter']
-    collection = db['redirects']
     generate_resourse_id = ''.join(random.choice(string.ascii_letters + string.digits)for _ in range(6))
-    document = await collection.insert_one({'long_url': long_url, 'resourse_id': generate_resourse_id})
-    return web.Response(text=generate_resourse_id, content_type='text/plain')
+    document = await collection.insert_one(
+        {'long_url': long_url,
+         'resourse_id': generate_resourse_id})
+    return web.Response(text=generate_resourse_id,
+                        content_type='text/plain')
 
 
 async def redirecter(request):
     resourse_id = request.match_info['resourse_id']
-    client = motor.motor_asyncio.AsyncIOMotorClient(f'mongodb://root:example@{os.environ.get("DB_HOST","localhost")}:27017')
-    db = client['redirecter']
-    collection = db['redirects']
     document = await collection.find_one({'resourse_id': resourse_id})
     if document is None:
         return web.Response(text="Not found", status=404)
---
+++ telegramm_bot.py
@@ -0,0 +1,66 @@
+import asyncio
+import random
+import string
+import os
+import motor.motor_asyncio
+from aiogram import Bot, Dispatcher, types
+from db_tktlg import TK_TG
+
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN', TK_TG)
+client = motor.motor_asyncio.AsyncIOMotorClient(
+        f'mongodb://root:example@{os.environ.get("DB_HOST", "localhost")}:27017')
+db = client['redirecter']
+collection = db['redirects']
+
+
+async def start_handler(event: types.Message):
+    await event.answer(
+        f"Hello, {event.from_user.get_mention(as_html=True)} Please enter the command: (/get) - to see the list of documents in your database (or empty) "
+        f"then enter your link (document) to generate it and write it to your database👋!",
+        parse_mode=types.ParseMode.HTML,
+    )
+
+
+async def add_URLs_handler(event: types.Message):
+    long_url = event.text
+    user_id = event.from_user.id
+    generate_resourse_id = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+    document = await collection.insert_one(
+        {'long_url': long_url,
+         'resourse_id': generate_resourse_id,
+         'user_id': user_id})
+    await event.answer(f"Link generated: {generate_resourse_id}")
+
+
+async def get_user_URLs_handler(event: types.Message):
+    user_id = event.from_user.id
+    documents = await collection.find({'user_id': user_id}).to_list(length=100)
+    if documents:
+        for document in documents:
+            await event.answer(document['long_url'])
+    else:
+        await event.answer("Sorry, there are no documents in your database")
+
+
+async def get_URL_handler(event: types.Message):
+    document = await collection.find_one({'resourse_id': event.text})
+    if document is None:
+        await event.answer('Sorry,no found')
+    long_url = document['long_url']
+    await event.answer(long_url)
+
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    try:
+        disp = Dispatcher(bot=bot)
+        disp.register_message_handler(start_handler, commands={"start", "restart"})
+        disp.register_message_handler(add_URLs_handler, regexp=r'http(s)?://.*')
+        disp.register_message_handler(get_user_URLs_handler, commands={'get'})
+        disp.register_message_handler(get_URL_handler, regexp=r'\w+')
+        await disp.start_polling()
+    finally:
+        await bot.close()
+
+asyncio.run(main())
