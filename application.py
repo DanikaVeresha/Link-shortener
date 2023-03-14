6b2162cc28795e25907c18af0376d960ce8dbdfb
daria.ch.prozorro <daria.ch.prozorro@gmail.com>
2023-03-14 09:00:44 +0200

    add_session

--- application.py
+++ application.py
@@ -1,8 +1,12 @@
+import base64
 import os
 import random
 import string
 from aiohttp import web
 import motor.motor_asyncio
+from cryptography import fernet
+from aiohttp_session import setup, get_session, session_middleware
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
     f'mongodb://root:example@{os.environ.get("DB_HOST","localhost")}:27017')
@@ -16,7 +20,19 @@ def html_response(document):
 
 
 async def index_page(request):
+    session = await get_session(request)
+    username_in_session = None
+    if 'user' in session:
+        username_in_session = session['user']
     return html_response('redirecter.html')
+
+
+async def login_page(request):
+    return html_response('login.html')
+
+
+async def register_page(request):
+    return html_response('register.html')
 
 
 async def recieve_url(request):
@@ -30,6 +46,34 @@ async def recieve_url(request):
                         content_type='text/plain')
 
 
+async def login_user(request):
+    data = await request.post()
+    username = data['username']
+    password = data['password']
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        f'mongodb://root:example@{os.environ.get("DB_HOST", "localhost")}:27017')
+    db = client['redirecter']
+    collection = db['users']
+    user_info = await collection.find_one({'username': username, 'password': password})
+    if user_info:
+        session = await get_session(request)
+        session['user'] = username
+        return web.Response(text="""<html>You have been logged in/<html>""")
+    return web.Response(text="""<html>Invalid username or password/<html>""")
+
+
+async def register_user(request):
+    data = await request.post()
+    username = data['username']
+    password = data['password']
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        f'mongodb://root:example@{os.environ.get("DB_HOST", "localhost")}:27017')
+    db = client['redirecter']
+    collection = db['users']
+    await collection.insert_one({'username': username, 'password': password})
+    return web.Response(text="""<html>You have been registered/<html>""")
+
+
 async def redirecter(request):
     resourse_id = request.match_info['resourse_id']
     document = await collection.find_one({'resourse_id': resourse_id})
@@ -38,9 +82,18 @@ async def redirecter(request):
     long_url = document['long_url']
     return web.HTTPFound(long_url)
 
+
 app = web.Application()
+fernet_key = fernet.Fernet.generate_key()
+secret_key = base64.urlsafe_b64decode(fernet_key)
+setup(app, EncryptedCookieStorage(secret_key))
+
 app.add_routes([web.get('/', index_page)])
 app.add_routes([web.post('/', recieve_url)])
+app.add_routes([web.get('/login', login_page)])
+app.add_routes([web.post('/login', login_user)])
+app.add_routes([web.get('/register', register_page)])
+app.add_routes([web.post('/register', register_user)])
 app.add_routes([web.get('/{resourse_id}', redirecter)])
 web.run_app(app)
 
